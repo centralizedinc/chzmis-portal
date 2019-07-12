dcc76a5a1b4f9f0fb5dc06fd85a314c36347bd3e
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-07-12 11:12:25 +0800

    first deployment with ant vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './plugins/ant-design-vue.js'
 
 Vue.config.productionTip = false
 
