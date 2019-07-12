78a289a6861a62e746d4a52029bb1e366ab1a049
Kris Bernard Yango <krisbernardyango@Kriss-MacBook-Pro.local>
2019-07-12 14:30:12 +0800

    chzmis

---
+++ src/views/landing/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <a-row>
+      <a-col :span="6" :offset="12">
+        <a-card>
+          <a-input placeholder="Username"/></a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card>
+          <a-input placeholder="Password"></a-input></a-card>
+      </a-col>
+    </a-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
