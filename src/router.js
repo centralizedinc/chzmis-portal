c0ee7dbe04b7f9e28c6b6035934d79b6a9a84bd9
WanDelaCruz <filipinashomenetwork@gmail.com>
2019-09-12 17:46:36 +0800

    interest, channel

--- src/router.js
+++ src/router.js
@@ -16,96 +16,105 @@ function isAuthenticated(to, from, next) {
 
 export default new Router({
   routes: [{
-    path: '/',
-    name: 'Landing Page',
-    component: MainLayout,
-    children: [{
-      path: '',
-      name: 'Home',
+      path: '/',
+      name: 'Landing Page',
+      component: MainLayout,
+      children: [{
+          path: '',
+          name: 'Home',
+          beforeEnter: isAuthenticated,
+          component: () => import('./views/landing/Home.vue')
+        },
+        // {
+        //   path: 'signUp',
+        //   name: 'Sign Up',
+        //   component: () => import('./views/landing/SignUpNew.vue')
+        // },
+        // {
+        //   path: 'signUp2',
+        //   name: 'Sign Up',
+        //   component: () => import('./views/landing/SignUp2.vue')
+        // },
+        // {
+        //   path: 'signUp3',
+        //   name: 'Sign Up',
+        //   component: () => import('./views/landing/SignUp3.vue')
+        // },
+        {
+          path: 'newAccount',
+          name: 'New Account',
+          beforeEnter: isAuthenticated,
+          component: () => import('./views/landing/CreateNewAccount.vue')
+        },
+        {
+          path: 'confirmRegistration',
+          name: 'Confirm Registration',
+          component: () => import('./views/landing/ConfirmRegistration.vue')
+        },
+        {
+          path: 'googleSignUp',
+          name: 'Google Sign up',
+          component: () => import('./views/landing/GoogleSignupSuccess.vue')
+        },
+        {
+          path: 'policy',
+          name: 'Google Policy',
+          component: () => import('./views/landing/Policy.vue')
+        },
+        {
+          path: 'facebookSignUp',
+          name: 'Facebook Sign up',
+          component: () => import('./views/landing/FacebookSignupSuccess.vue')
+        },
+        {
+          path: 'interest',
+          name: 'Choose Interest',
+          component: () => import('./views/landing/ChooseInterest.vue')
+        },
+        // {
+        //   path: 'registration',
+        //   name: 'Registration',
+        //   component: () => import('./views/landing/RegForm.vue')
+        // },
+        // {
+        //   path: 'login',
+        //   name: 'Login',
+        //   component: () => import('./views/landing/Login.vue')
+        // },
+        {
+          path: 'create/connection',
+          name: 'Create Connections',
+          component: () => import('./views/landing/CreateConnection.vue')
+        },
+        {
+          path: 'search/connection',
+          name: 'Connect with Others',
+          component: () => import('./components/SearchConnection.vue')
+        } {
+          path: 'create/channel',
+          name: 'Channel ek ek',
+          component: () => import('./components/CreateChannel.vue')
+        }
+      ]
+    },
+    {
+      path: '/main',
+      name: 'Secured Layout',
+      component: SecuredLayout,
       beforeEnter: isAuthenticated,
-      component: () => import('./views/landing/Home.vue')
-    },
-    // {
-    //   path: 'signUp',
-    //   name: 'Sign Up',
-    //   component: () => import('./views/landing/SignUpNew.vue')
-    // },
-    // {
-    //   path: 'signUp2',
-    //   name: 'Sign Up',
-    //   component: () => import('./views/landing/SignUp2.vue')
-    // },
-    // {
-    //   path: 'signUp3',
-    //   name: 'Sign Up',
-    //   component: () => import('./views/landing/SignUp3.vue')
-    // },
-    {
-      path: 'newAccount',
-      name: 'New Account',
-      beforeEnter: isAuthenticated,
-      component: () => import('./views/landing/CreateNewAccount.vue')
+      children: [{
+        path: '',
+        name: 'Main Page',
+        component: () => import('./views/main/MainPage.vue')
+      }]
     },
     {
-      path: 'confirmRegistration',
-      name: 'Confirm Registration',
-      component: () => import('./views/landing/ConfirmRegistration.vue')
-    },
-    {
-      path: 'googleSignUp',
-      name: 'Google Sign up',
-      component: () => import('./views/landing/GoogleSignupSuccess.vue')
-    },
-    {
-      path: 'policy',
-      name: 'Google Policy',
-      component: () => import('./views/landing/Policy.vue')
-    },
-    {
-      path: 'facebookSignUp',
-      name: 'Facebook Sign up',
-      component: () => import('./views/landing/FacebookSignupSuccess.vue')
-    },
-    // {
-    //   path: 'registration',
-    //   name: 'Registration',
-    //   component: () => import('./views/landing/RegForm.vue')
-    // },
-    // {
-    //   path: 'login',
-    //   name: 'Login',
-    //   component: () => import('./views/landing/Login.vue')
-    // },
-    {
-      path: 'create/connection',
-      name: 'Create Connections',
-      component: () => import('./views/landing/CreateConnection.vue')
-    },
-    {
-      path: 'search/connection',
-      name: 'Connect with Others',
-      component: () => import('./components/SearchConnection.vue')
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
-    ]
-  },
-  {
-    path: '/main',
-    name: 'Secured Layout',
-    component: SecuredLayout,
-    beforeEnter: isAuthenticated,
-    children: [{
-      path: '',
-      name: 'Main Page',
-      component: () => import('./views/main/MainPage.vue')
-    }]
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
-  }
   ]
 })
